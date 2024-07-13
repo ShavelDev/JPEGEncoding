950974f3817176ba7fdb5b6b7971de4867a5c04e
ShavelDev <nor.szaw@gmail.com>
2024-07-13 17:32:55 +0200

    Initial Commit

---
+++ JPEGEncoding/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JPEGEncoding
+//
+//  Created by Norbert Szawloga on 13/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
